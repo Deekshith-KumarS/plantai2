717f34b7fbd578765f1bba9de5d0911c079ba40c
Deekshith-KumarS <deekshithkumars2006@gmail.com>
2026-06-01 21:36:55 +0530

    Force mongoose to use IPv4 to fix Vercel timeout bug

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      family: 4, // Force IPv4 to fix Vercel Serverless timeout issues
+    });
 
     console.log("MongoDB Connected");
   } catch (error) {
